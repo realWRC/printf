1c65939c9e14954f27df684ec6801b79b05cf255
Wongani <wonganichulu@gmail.com>
2023-12-04 21:46:29 +0200

    Added print_int function to function.c

--- function.c
+++ function.c
@@ -17,3 +17,23 @@ int _strlen(char *s)
 	}
 	return (length);
 }
+/**
+ * print_int - function that prints an integer with write function
+ * @integer: Integer to be printed
+ */
+int print_int(int integer)
+{
+	int size = snprintf(NULL, 0, "%d", integer);
+	char *container;
+
+	container = (char *)malloc((size + 1) * sizeof(char));
+	if (container == NULL)
+	{
+		free(container);
+		exit(EXIT_FAILURE);
+	}
+	snprintf(container, size + 1, "%d", integer);
+	write(1, container, size);
+	free(container);
+	return (size);
+}

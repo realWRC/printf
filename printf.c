9300b5187307ffcd56fa10555069f325a74e83a3
Wongani <wonganichulu@gmail.com>
2023-12-05 10:20:18 +0200

    Bug fix 2

--- printf.c
+++ printf.c
@@ -14,7 +14,7 @@ int _printf(const char *format, ...)
 	int i, j;
 
 	va_start(str, format);
-	for (i = 0, j = 0;format[i] != '\0'; i++)
+	for (i = 0, j = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 		{
